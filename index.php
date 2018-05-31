59d38412c6d154f5c5119e23e934828e09f7d9a8
david-thewissen <david.thewissen@gmail.com>
2018-05-31 13:49:15 +0200

    test

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php include_once("index.html");
+if (isset($_POST['submit'])) {
+  print_r($_POST);
+  $nom=$_POST['nom'];
+  $email=$_POST['mail'];
+  $message=$_POST['message'];
+
+
+  $to = "david.thewissen@gmail.com";
+  $message = "Nom: ".$_POST['nom']."\r\n";
+  $message .= 'E-mail: '.$_POST['mail']."\r\n";
+  $message .= "Message: \r\n\r\n\r\n" .$_POST['message']."\r\n\r\n--\r\n\r\n";
+  $headers = 'From: client@example.com' . "\r\n" .
+  'Reply-To: client@example.com' . "\r\n" .
+  'X-Mailer: PHP/' . phpversion();
+
+  mail($to, $subject, $message, $headers);
+
+ ?>
